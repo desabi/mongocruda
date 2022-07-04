d2bc79e79b45d8f9e61b6b640490c195be78b361
Abigahil Delgado <abigahil.delgado@ine.mx>
2022-07-04 18:12:40 -0500

    Se agrega crud

---
+++ src/main/java/com/abi/mongocruda/controller/PersonController.java
@@ -0,0 +1,53 @@
+package com.abi.mongocruda.controller;
+
+import com.abi.mongocruda.entity.Person;
+import com.abi.mongocruda.model.PersonRequest;
+import com.abi.mongocruda.service.PersonService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    // create
+    @PostMapping("")
+    public ResponseEntity<Person> create(@Valid @RequestBody PersonRequest personRequest) {
+        Person personSaved = personService.save(personRequest);
+        return new ResponseEntity<>(personSaved, HttpStatus.OK) ;
+    }
+
+    // read
+    @GetMapping
+    public ResponseEntity<List<Person>> read() {
+        List<Person> all = personService.findAll();
+        return new ResponseEntity<>(all, HttpStatus.OK);
+    }
+
+    // read by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> read(@PathVariable("id") ObjectId id) {
+        return personService.findById(id);
+    }
+
+    // update
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> update(@PathVariable("id") ObjectId id, @Valid @RequestBody PersonRequest personRequest) {
+        return personService.update(id, personRequest);
+    }
+
+    // delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") ObjectId id) {
+        return personService.delete(id);
+    }
+}
